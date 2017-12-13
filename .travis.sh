aaeaf72ea959b7d60c1b57cf1262c5ca3cf90ca2
Jason Kingsbury <jkingsbury@ukch-ofclt2695.illumina.com>
2017-12-13 11:48:44 +0000

    use correct cayley syntax, wait for output from cayley instead of sleep

--- .travis.sh
+++ .travis.sh
@@ -2,5 +2,6 @@
 wget https://github.com/google/cayley/releases/download/v0.6.1/cayley_v0.6.1_linux_amd64.tar.gz
 tar -xvzf cayley_v0.6.1_linux_amd64.tar.gz
 cd cayley_v0.6.1_linux_amd64
-./cayley http -i=30kmoviedata.nq.gz &
-sleep 2
+exec 3< <(./cayley http -dbpath=data/30kmoviedata.nq.gz)
+read <&3
+exec 3<&-

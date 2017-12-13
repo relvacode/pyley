3514d26acbed31e9f4e71fe053a982d3bca41f2c
Jason Kingsbury <jkingsbury@ukch-ofclt2695.illumina.com>
2017-12-13 11:39:23 +0000

    Cayley 0.6.1, fix test download URL

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://github.com/google/cayley/releases/download/v0.6.0/cayley_0.6.0_linux_amd64.tar.gz
-tar -xvzf cayley_0.6.0_linux_amd64.tar.gz
-cd cayley_0.6.0_linux_amd64
+wget https://github.com/google/cayley/releases/download/v0.6.1/cayley_v0.6.1_linux_amd64.tar.gz
+tar -xvzf cayley_v0.6.1_linux_amd64.tar.gz
+cd cayley_v0.6.1_linux_amd64
 ./cayley http -i=30kmoviedata.nq.gz &
 sleep 2
